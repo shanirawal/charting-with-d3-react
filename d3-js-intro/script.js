aac2577dc0e0f3d0f9fc0d9f74d40742c0d54bc8
shanirawal <shanirawal5@gmail.com>
2025-07-06 16:45:18 +0530

    Session 3:Data joins and transitions

--- d3-js-intro/script.js
+++ d3-js-intro/script.js
@@ -24,3 +24,58 @@ d3.select("#interactive-area").select(":first-child").remove()
 
 //select div and add elemnts after last child
 d3.select("#interactive-area").insert("p",":last-child").text("Insert a p tag")
+
+//d3 joins - INCOMPLTE , NEED TO ADD JOINS
+
+const datajoinarea= d3.select("#data-join-area");
+
+let initiateData = [10,20];
+
+const divs1=datajoinarea.selectAll("div").data(initiateData)
+
+divs1
+    .enter()
+    .append("div")
+    .text((d)=>`Initial ${d}`);
+
+
+
+let updatedData = [10,30,40,50];
+const divs2=datajoinarea.selectAll("div").data(updatedData);
+
+
+divs2
+    .enter()
+    .append("div")
+    .text((d)=>`Initial ${d}`)
+    .style("background-color","red");
+    //result -[10,20,40,50]
+
+// data transitions
+
+const transitionArea = d3.select("#transition-area");
+
+function runTransition(){
+    transitionArea.html(""); //clear all html elememts inside selected div
+
+    const mySvg = transitionArea
+        .append("svg")
+        .attr("width","100%")
+        .attr("height","200");
+
+    mySvg
+        .selectAll("circle")
+        .data([20,40,60,80,100])
+        .enter()
+        .append("circle")
+        .attr("cx",0)
+        .attr("cy",(d,i)=> 25 + i  * 20)
+        .attr("r",10)
+        .style("fill","blue")
+        .transition()
+        .duration(1500)
+        .attr("cx",(d)=> d * 3)
+        .attr("cy",(d)=> d * 3);
+
+
+}
